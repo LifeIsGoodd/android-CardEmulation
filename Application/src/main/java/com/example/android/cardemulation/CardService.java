ca771b64fd3c1cbfe39a473807ce16854c6557cb
LifeIsGoodd <86762679+LifeIsGoodd@users.noreply.github.com>
2026-07-28 11:22:32 +0300

    Update CardService.java

--- Application/src/main/java/com/example/android/cardemulation/CardService.java
+++ Application/src/main/java/com/example/android/cardemulation/CardService.java
@@ -49,7 +49,7 @@ public class CardService extends HostApduService {
     // "OK" status word sent in response to SELECT AID command (0x9000)
     private static final byte[] SELECT_OK_SW = HexStringToByteArray("9000");
     // "UNKNOWN" status word sent in response to invalid APDU command (0x0000)
-    private static final byte[] UNKNOWN_CMD_SW = HexStringToByteArray("0000");
+    private static final byte[] UNKNOWN_CMD_SW = HexStringToByteArray("6D00");
     private static final byte[] SELECT_APDU = BuildSelectApdu(SAMPLE_LOYALTY_CARD_AID);
     private static final byte[] VERIFY_APDU = new byte[] {(byte)0x80, (byte)0x20, (byte)0x00, (byte)0x00};
 
@@ -118,10 +118,11 @@ public class CardService extends HostApduService {
     */
   @Override
   public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
-      Log.i(TAG, "Received APDU: " + ByteArrayToHexString(commandApdu));
+      Log.i(TAG, "-> Received APDU: " + ByteArrayToHexString(commandApdu));
 
       // Проверка 1: Если пришла стандартная команда SELECT AID (уже была в проекте)
       if (cmp_array(SELECT_APDU, commandApdu, SELECT_APDU.length)) {
+        Log.i(TAG, " Cmd SELECT_APDU " + account);  
         String account = AccountStorage.GetAccount(this);
         // Защита от нечётности: если строка нечётная, дописываем '0' в самый перед
         if (account.length() % 2 != 0) {
@@ -139,14 +140,14 @@ public class CardService extends HostApduService {
                                 + Character.digit(account.charAt(i+1), 16));
         }          
           
-        Log.i(TAG, "Sending account number: " + account);
+        Log.i(TAG, "<- Sending account number: " + account + " " + ByteArrayToHexString(SELECT_OK_SW));
         return ConcatArrays(accountBytes, SELECT_OK_SW); // Возвращает данные + 90 00
       }
     
       // Проверка 2: Наша новая кастомная команда (Запрос-Ответ)
       // Проверяем первые 4 байта входящего пакета (Класс, Инструкция, P1, P2)
       else if (cmp_array(VERIFY_APDU, commandApdu, VERIFY_APDU.length)) {
-        Log.i(TAG, "Command VERIFY: " + ByteArrayToHexString(commandApdu));
+        Log.i(TAG, "  Cmd VERIFY");
         
         // Извлекаем полезные данные, которые прислал ридер (начиная с 5-го байта)
         // commandApdu[4] - это байт длины данных Lc
@@ -163,7 +164,7 @@ public class CardService extends HostApduService {
         for (int i = 0, j = dataLength - 1; i < dataLength; i++, j--) {
           successPayload[i] = receivedData[j];
         }
-        Log.i(TAG, "Answer VERIFY: " + ByteArrayToHexString(successPayload));  
+        Log.i(TAG, "<- Answer VERIFY: " + ByteArrayToHexString(successPayload) + " " + ByteArrayToHexString(SELECT_OK_SW));  
         return ConcatArrays(successPayload, SELECT_OK_SW);
 
         /*
@@ -188,8 +189,9 @@ public class CardService extends HostApduService {
 
       // Если ридер прислал любую другую неизвестную команду
       else {
-        Log.w(TAG, "Неизвестная APDU команда");
-        return UNKNOWN_CMD_SW; // Возвращает статус ошибки 0x00 0x00 или 0x6F 0x00
+        Log.w(TAG, "  Неизвестная APDU команда");
+        Log.w(TAG, "<- " + ByteArrayToHexString(UNKNOWN_CMD_SW));
+        return UNKNOWN_CMD_SW; // Возвращает статус ошибки 0x00 0x00 или 0x6D 0x00 или 0x6F 0x00
       }
 }
 
